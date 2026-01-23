ca53a5a610298253802249ea021d1e262e9c0efa
Wlfjd <leejiwon813@naver.com>
2026-01-23 23:42:12 +0900

    fix: remove unused files

--- src/shared/components/bottom-nav-bar/BgrBottomNavBar.tsx
+++ src/shared/components/bottom-nav-bar/BgrBottomNavBar.tsx
@@ -6,7 +6,7 @@ const BOTTOM_NAV_BAR_ITEMS = [
 ]
 function BgrBottomNavBar() {
     return (
-        <footer className="flex bg-red-500 w-full items-center justify-end gap-2 h-[104px] p-6">
+        <footer className="flex w-full items-center justify-end gap-2 h-[104px] p-6">
           {BOTTOM_NAV_BAR_ITEMS.map((item) => (
             <BgrButton key={item.label} title={item.label} size='lg' variant={item.variant as 'primary-filled' | 'primary-outlined'} />       
           ))}           
