186847628ce81814c29ce00487de23ef340ccfd8
yeaseula <kminea@naver.com>
2026-02-27 19:31:22 +0900

    상단 입력 step 퍼블리싱 완료

--- src/pages/products/create/create-page.tsx
+++ src/pages/products/create/create-page.tsx
@@ -1,5 +1,129 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from '@/shared/components/ui/accordion/accordion'
+import { StageTab } from '@/shared/components/ui/tab/stage-tab'
+import { Tooltip } from '@/shared/components/ui/tooltip/tooltip'
+import Chip from '@/shared/components/ui/chip/chip'
+import { Info, ChevronDown } from 'lucide-react'
+
+const stagestep = [
+  '상품 정보',
+  '배송 정보',
+  '썸네일 등록',
+  '상품 옵션 정보',
+  '상세페이지 등록',
+  '상품 정보 제공 고시',
+]
+
 function CreatePage() {
-  return <div>CreatePage</div>
+  return (
+    <>
+      <div className="sticky top-0 left-0 -mt-40 -ml-[90px] w-[calc(100%+180px)] border-b border-b-gray-200 bg-white px-[90px] pt-16">
+        <div className="flex w-full items-center justify-between">
+          <StageTab
+            currentStep={1}
+            steps={stagestep}
+            className="w-fit justify-start border-none"
+          />
+          <p className="typo-title-16-sb">
+            필수 입력 사항이 <span className="text-primary-500">5개</span>{' '}
+            남았어요
+          </p>
+        </div>
+
+        <Accordion type="single" collapsible defaultValue={'step'}>
+          <AccordionItem value="step" className="relative">
+            <AccordionContent className="h-auto border-t border-gray-200 pb-24">
+              <div className="mt-8 grid grid-cols-2 gap-30 pt-12">
+                <div>
+                  <div className="flex gap-2">
+                    <p className="typo-heading-18-b">필수성분</p>
+                    <span className="typo-title-16-m text-primary-500">*</span>
+                    <Tooltip position="bottom">
+                      <Tooltip.Trigger>
+                        <Info size={24} />
+                      </Tooltip.Trigger>
+                      <Tooltip.Content className="px-8 py-6">
+                        <p>
+                          상품 등록을 위해선 아래 5가지 기준 중<br></br> 하나
+                          이상 충족해야 합니다.
+                        </p>
+                      </Tooltip.Content>
+                    </Tooltip>
+                  </div>
+                  <div className="mt-10 flex gap-4">
+                    <Tooltip position="bottom">
+                      <Tooltip.Trigger>
+                        <Chip size="md">#글루텐프리</Chip>
+                      </Tooltip.Trigger>
+                      <Tooltip.Content className="px-8 py-6">
+                        <p>밀·보리·호밀 등 글루텐 함유 곡물 미포함</p>
+                      </Tooltip.Content>
+                    </Tooltip>
+                    <Tooltip position="bottom">
+                      <Tooltip.Trigger>
+                        <Chip size="md">#글루텐프리</Chip>
+                      </Tooltip.Trigger>
+                      <Tooltip.Content className="px-8 py-6">
+                        <p>밀·보리·호밀 등 글루텐 함유 곡물 미포함</p>
+                      </Tooltip.Content>
+                    </Tooltip>
+                    <Tooltip position="bottom">
+                      <Tooltip.Trigger>
+                        <Chip size="md">#글루텐프리</Chip>
+                      </Tooltip.Trigger>
+                      <Tooltip.Content className="px-8 py-6">
+                        <p>밀·보리·호밀 등 글루텐 함유 곡물 미포함</p>
+                      </Tooltip.Content>
+                    </Tooltip>
+                  </div>
+                </div>
+                <div>
+                  <p className="typo-heading-18-b">적용된 카테고리</p>
+                  <div className="mt-10 flex gap-4">
+                    <Tooltip position="bottom">
+                      <Tooltip.Trigger>
+                        <Chip size="md">#글루텐프리</Chip>
+                      </Tooltip.Trigger>
+                      <Tooltip.Content className="px-8 py-6">
+                        <p>밀·보리·호밀 등 글루텐 함유 곡물 미포함</p>
+                      </Tooltip.Content>
+                    </Tooltip>
+                    <Tooltip position="bottom">
+                      <Tooltip.Trigger>
+                        <Chip size="md">#글루텐프리</Chip>
+                      </Tooltip.Trigger>
+                      <Tooltip.Content className="px-8 py-6">
+                        <p>밀·보리·호밀 등 글루텐 함유 곡물 미포함</p>
+                      </Tooltip.Content>
+                    </Tooltip>
+                    <Tooltip position="bottom">
+                      <Tooltip.Trigger>
+                        <Chip size="md">#글루텐프리</Chip>
+                      </Tooltip.Trigger>
+                      <Tooltip.Content className="px-8 py-6">
+                        <p>밀·보리·호밀 등 글루텐 함유 곡물 미포함</p>
+                      </Tooltip.Content>
+                    </Tooltip>
+                  </div>
+                </div>
+              </div>
+            </AccordionContent>
+            <AccordionTrigger className="absolute right-[90px] -bottom-48 justify-center border px-0 py-0">
+              <div className="rounded-b-16 bg-white px-[26px] py-6">
+                <ChevronDown size={36}></ChevronDown>
+              </div>
+            </AccordionTrigger>
+          </AccordionItem>
+        </Accordion>
+      </div>
+
+      <div className="mt-[88px] h-[1000px]">Create Page</div>
+    </>
+  )
 }
 
 export default CreatePage
