b348bf4fba29be8d22fa48617b236b9ccced3ccd
yeaseula <kminea@naver.com>
2026-02-27 23:35:11 +0900

    sticky header 컴포넌트 분리

---
+++ src/pages/products/create/blocks/option-tags-area.tsx
@@ -0,0 +1,45 @@
+import { Tooltip } from '@/shared/components/ui/tooltip/tooltip'
+import { Info } from 'lucide-react'
+import { OptionTags } from '@/entity/products/create/options-tag.type'
+import Chip from '@/shared/components/ui/chip/chip'
+import { cn } from '@/shared/lib/utils'
+
+interface OptionTagsAreaProps {
+  title: string
+  titleRequire?: boolean
+  titleTooltipProps?: React.ReactNode
+  tagData: OptionTags[]
+  className?: string
+}
+
+export const OptionTagsArea = ({
+  title,
+  titleRequire,
+  titleTooltipProps,
+  tagData,
+  className,
+}: OptionTagsAreaProps) => {
+  return (
+    <div>
+      <div className={cn('flex gap-2', className)}>
+        <p className="typo-heading-18-b">{title}</p>
+        {titleRequire && (
+          <span className="typo-title-16-m text-primary-500">*</span>
+        )}
+        {titleTooltipProps}
+      </div>
+      <div className="mt-10 flex gap-4">
+        {tagData.map((items) => (
+          <Tooltip position="bottom" key={items.title}>
+            <Tooltip.Trigger>
+              <Chip size="md">#{items.title}</Chip>
+            </Tooltip.Trigger>
+            <Tooltip.Content className="px-8 py-6">
+              <p>{items.tooltip}</p>
+            </Tooltip.Content>
+          </Tooltip>
+        ))}
+      </div>
+    </div>
+  )
+}
