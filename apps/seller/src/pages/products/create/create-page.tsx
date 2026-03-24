f65d116f97c88333f8286967ad20e41051f0bf82
yeaseula <kminea@naver.com>
2026-03-24 16:58:19 +0900

    ♻️ Create-page 버튼 미구현 함수 부분 주석 추가

--- apps/seller/src/pages/products/create/create-page.tsx
+++ apps/seller/src/pages/products/create/create-page.tsx
@@ -50,6 +50,7 @@ function CreatePageInner() {
       </div>
 
       <div className="flex gap-12">
+        {/* TODO: 후속 작업 필요  - 함수 미구현 상태 입니다 */}
         <Button title="미리보기" variant="primary-outlined" size="lg" />
         <Button title="임시저장" variant="primary-outlined" size="lg" />
         <Button title="저장하기" variant="primary-filled" size="lg" />
