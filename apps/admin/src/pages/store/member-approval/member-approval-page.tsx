820ace38a6e80ca2c8d237e9f5737dc05dfafb23
yeaseula <kminea@naver.com>
2026-04-11 21:04:43 +0900

    [admin] feat(155):회원가입 승인 테이블 제외 퍼블리싱

--- apps/admin/src/pages/store/member-approval/member-approval-page.tsx
+++ apps/admin/src/pages/store/member-approval/member-approval-page.tsx
@@ -1,3 +1,31 @@
+import { Button, Pagination, Table } from '@dessert/ui'
+
 export function MemberApprovalPage() {
-  return <div>MemberApproval</div>
+  return (
+    <section className="rounded-10 border border-gray-300 bg-white">
+      <div className="flex items-center justify-between px-24 pt-16">
+        <div className="flex gap-16">
+          {/* 버튼 */}
+          <div className="flex gap-10">
+            <Button title="승인" variant="primary-outlined" />
+            <Button title="서류 다운로드" variant="secondary-outlined" />
+          </div>
+          {/* 선택갯수 */}
+          <div className="flex items-center gap-4">
+            <p className="typo-title-14-r text-gray-700">
+              선택 <span className="typo-title-14-m text-primary-500">2개</span>
+            </p>
+            <span className="h-12 w-2 bg-gray-400" />
+            <p className="typo-title-14-r text-gray-700">
+              전체 <span className="typo-title-14-m text-primary-500">4개</span>
+            </p>
+          </div>
+        </div>
+        {/* 페이지네이션 */}
+        <Pagination currentPage={1} totalPages={2} />
+      </div>
+
+      <div className="mt-12"></div>
+    </section>
+  )
 }
