b0421fb2dd1ef983a7cd4681b6f6ee41b41cf12e
yeaseula <kminea@naver.com>
2026-04-15 18:08:09 +0900

    [admin] fix(145):navigation 중복 호출 로직 수정

--- apps/admin/src/features/auth/logout.mutation.ts
+++ apps/admin/src/features/auth/logout.mutation.ts
@@ -1,5 +1,4 @@
 import { useMutation } from '@tanstack/react-query'
-import { useNavigate } from 'react-router-dom'
 
 import { adminLogout, authKeys, useAuthStore } from '@/entity/auth'
 import { TOKEN_COOKIE_KEYS } from '@/shared/constant'
@@ -7,7 +6,6 @@ import { deleteCookie } from '@/shared/utils'
 
 export const useAdminLogoutMutation = () => {
   const setIsLoggedIn = useAuthStore((state) => state.setIsLoggedIn)
-  const navigate = useNavigate()
 
   return useMutation({
     mutationKey: authKeys.all,
@@ -16,7 +14,6 @@ export const useAdminLogoutMutation = () => {
       deleteCookie(TOKEN_COOKIE_KEYS.ACCESS)
       deleteCookie(TOKEN_COOKIE_KEYS.REFRESH)
       setIsLoggedIn(false)
-      navigate('/login')
     },
   })
 }
