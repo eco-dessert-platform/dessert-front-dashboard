622d268fda1d931c1a118a50524a24970822a44a
yeaseula <kminea@naver.com>
2026-04-17 20:31:46 +0900

    [admin] fix(145): logout 호출 재사용

--- apps/admin/src/features/auth/logout.mutation.ts
+++ apps/admin/src/features/auth/logout.mutation.ts
@@ -1,19 +1,13 @@
 import { useMutation } from '@tanstack/react-query'
 
 import { adminLogout, authKeys, useAuthStore } from '@/entity/auth'
-import { TOKEN_COOKIE_KEYS } from '@/shared/constant'
-import { deleteCookie } from '@/shared/utils'
 
 export const useAdminLogoutMutation = () => {
-  const setIsLoggedIn = useAuthStore((state) => state.setIsLoggedIn)
+  const logout = useAuthStore((state) => state.logout)
 
   return useMutation({
     mutationKey: authKeys.all,
     mutationFn: adminLogout,
-    onSuccess: () => {
-      deleteCookie(TOKEN_COOKIE_KEYS.ACCESS)
-      deleteCookie(TOKEN_COOKIE_KEYS.REFRESH)
-      setIsLoggedIn(false)
-    },
+    onSuccess: logout,
   })
 }
