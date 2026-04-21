2d3522f08f0432d4aef4dbe8c46ac9de8cc61f2f
yeaseula <kminea@naver.com>
2026-04-21 17:41:45 +0900

    [seller] fix(173): Chip 변경된 디자인 시안 반영

--- packages/ui/src/chip/chip.tsx
+++ packages/ui/src/chip/chip.tsx
@@ -31,7 +31,7 @@ const Chip = ({
 
   const sizeClasses = {
     sm: 'px-2 py-1 typo-body-10-r rounded-full',
-    md: 'px-3 py-1.5 typo-body-12-r rounded-full',
+    md: 'px-3 py-1.5 typo-body-14-r rounded-full',
   }
 
   const closeClasses = {
