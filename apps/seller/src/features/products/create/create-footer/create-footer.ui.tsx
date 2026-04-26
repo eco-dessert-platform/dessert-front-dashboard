8cf0346313ac684006e8ed2f5340338bd100ec55
yeaseula <kminea@naver.com>
2026-04-26 12:24:01 +0900

    [seller] feat(173): 미리보기, 저장하기 버튼 form 입력시에만 활성화되도록 수정

--- apps/seller/src/features/products/create/create-footer/create-footer.ui.tsx
+++ apps/seller/src/features/products/create/create-footer/create-footer.ui.tsx
@@ -1,12 +1,19 @@
 import { Button } from '@dessert/ui'
+import { useFormContext } from 'react-hook-form'
 
+import { CreateFormType } from '@/entity/products/create/create-form'
 import { useSubmitCreateForm } from '@/features/products/create/create-form'
+
 interface ProductFooterProps {
   onPreview: () => void
 }
 
 export const ProductFooter = ({ onPreview }: ProductFooterProps) => {
   const { handleSubmit, isPending } = useSubmitCreateForm()
+  const {
+    formState: { isDirty },
+  } = useFormContext<CreateFormType>()
+  const hasAnyInput = isDirty
 
   return (
     <div className="sticky bottom-0 left-0 z-20 -mb-36 -ml-[90px] flex w-[calc(100%+180px)] justify-end gap-12 border-t border-t-gray-200 bg-white px-[90px] py-24">
@@ -16,11 +23,17 @@ export const ProductFooter = ({ onPreview }: ProductFooterProps) => {
         size="lg"
         onClick={onPreview}
       />
-      <Button title="임시저장" variant="primary-outlined" size="lg" />
+      <Button
+        title="임시저장"
+        variant="primary-outlined"
+        size="lg"
+        disabled={!hasAnyInput}
+      />
       <Button
         title="저장하기"
         variant="primary-filled"
         size="lg"
+        disabled={!hasAnyInput}
         onClick={handleSubmit}
       />
     </div>
