6f9779986fc113258790e56c573996222fba8caa
yeaseula <kminea@naver.com>
2026-04-28 18:50:57 +0900

    [seller] feat(204): 유효성 로직 썸네일 스키마 추가

--- apps/seller/src/features/products/create/create-form/product-create.types.ts
+++ apps/seller/src/features/products/create/create-form/product-create.types.ts
@@ -10,14 +10,16 @@ import { deliverySchema } from '../create-form-delivery/create-delivery.schema'
 import { disclosureSchema } from '../create-form-disclosure/create-disclosure.schema'
 import { productSchema } from '../create-form-info/create-info.schema'
 import { productOptionSchema } from '../create-form-options/create-options.schema'
+import { thumbnailSchema } from '../create-form-thumbnail/create-form-thumbnail.schema'
 
 export type CreateProductForm = ProductFormInput &
   DeliveryFormInput &
+  ThumbnailFormInput &
   ProductOptionFormInput &
-  ProductDisclosureFormInput &
-  ThumbnailFormInput
+  ProductDisclosureFormInput
 
 export const createProductSchema = productSchema
   .and(deliverySchema)
+  .and(thumbnailSchema)
   .and(productOptionSchema)
   .and(disclosureSchema)
