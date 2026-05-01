7000eb057f83595b271b661cceeb00b7d1f97ceb
yeaseula <kminea@naver.com>
2026-05-01 15:37:11 +0900

    [seller] refactor(204):사용하지 않는 import 제거

--- apps/seller/src/features/products/create/create-form-thumbnail/create-form-thumbnail-area.ui.tsx
+++ apps/seller/src/features/products/create/create-form-thumbnail/create-form-thumbnail-area.ui.tsx
@@ -29,7 +29,7 @@ export const ThumbnailUploadArea = () => {
     setDeleteTarget,
     handleFileChange,
     handleImageDelete,
-    handleReorderExtraImages,
+
     handleDragEnd,
   } = useProductThumbnailForm()
 
