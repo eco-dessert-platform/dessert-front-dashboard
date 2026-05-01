69ce7005d4374f38899619c823e31d8310b1ac1f
yeaseula <kminea@naver.com>
2026-05-01 21:42:45 +0900

    [seller] fix(204):extraimage type변경에 다른 스키마 유효성검증 로직 수정

--- apps/seller/src/features/products/create/create-form-thumbnail/create-form-thumbnail.schema.ts
+++ apps/seller/src/features/products/create/create-form-thumbnail/create-form-thumbnail.schema.ts
@@ -1,11 +1,19 @@
 import { z } from 'zod'
 
 export const thumbnailSchema = z.object({
-  mainImage: z.instanceof(File, {
-    message: '대표 이미지는 필수 입력사항입니다.',
-  }),
+  mainImage: z
+    .instanceof(File, {
+      message: '대표 이미지는 필수 입력사항입니다.',
+    })
+    .nullable(),
+
   extraImages: z
-    .array(z.instanceof(File))
+    .array(
+      z.object({
+        id: z.string(),
+        file: z.instanceof(File),
+      }),
+    )
     .max(9, { message: '추가 이미지는 최대 9개까지 등록 가능합니다.' })
     .default([]),
 })
