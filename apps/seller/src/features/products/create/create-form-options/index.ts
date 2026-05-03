542c76b42d6bcb6c11d842d5cec1311d9153a326
yeaseula <kminea@naver.com>
2026-05-03 18:24:43 +0900

    [seller] refactor(215): Public API 잘못된 export 형식 수정

--- apps/seller/src/features/products/create/create-form-options/index.ts
+++ apps/seller/src/features/products/create/create-form-options/index.ts
@@ -3,12 +3,10 @@ export { useProductOptionForm } from './use-product-options.form.hook'
 export { productOptionSchema } from './create-options.schema'
 export { ProductOptionForm } from './create-form-options-form.ui'
 export type { ProductOptionsType } from './create-form-options.type'
-export type {
+export {
   MAIN_CATEGORY_OPTIONS,
   SUB_CATEGORY_MAP,
 } from './options-category.constant'
-export type {
-  NUTRITION_FIELDS,
-  NutritionFieldKey,
-} from './options-nutrition.constant'
-export type { SHIPPING_DAYS } from './options-shipping-days.constant'
+export { NUTRITION_FIELDS } from './options-nutrition.constant'
+export type { NutritionFieldKey } from './options-nutrition.constant'
+export { SHIPPING_DAYS } from './options-shipping-days.constant'
