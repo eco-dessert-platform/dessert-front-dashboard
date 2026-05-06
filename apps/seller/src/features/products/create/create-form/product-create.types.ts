8f9596ceea72b69a110199950fbc273c265d7564
yeaseula <kminea@naver.com>
2026-05-06 22:31:28 +0900

    [seller] refactor(228): 주석 제거

--- apps/seller/src/features/products/create/create-form/product-create.types.ts
+++ apps/seller/src/features/products/create/create-form/product-create.types.ts
@@ -10,10 +10,6 @@ import { productOptionSchema } from '../create-form-options/create-options.schem
 import { thumbnailSchema } from '../create-form-thumbnail/create-form-thumbnail.schema'
 import { ThumbnailFormType } from '../create-form-thumbnail/create-form-thumbnail.type'
 
-//ThumbnailFormType이 entity에 있지 않는 이유는
-//이전 PR에서 entity구조에 대한 FSD위반 코드리뷰를 받았기 때문입니다.
-//상단 @/entity/products 역시 파일 구조 수정 예정이니 참고 부탁드립니다.
-
 export type CreateFormType = ProductInfoType &
   ProductDeliveryType & {
     options: z.infer<typeof productOptionSchema>[]
