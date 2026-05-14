e07930e547a1b23d7048c03b2c1ee74e952b47c3
JangHwanPark <wkd5878@gmail.com>
2026-05-14 15:03:05 +0900

    [seller] fix(245): develop 머지 후속 정리 (blank screen, 검색 버튼, 푸터 wiring)

--- apps/seller/src/features/register/store-info/store-info.ui.tsx
+++ apps/seller/src/features/register/store-info/store-info.ui.tsx
@@ -5,14 +5,11 @@ import { ImageIcon } from '@dessert/icons'
 import { Controller, useFormContext, useWatch } from 'react-hook-form'
 import { useKakaoPostcodePopup } from 'react-daum-postcode'
 
-import {
-  FILE_UPLOAD_LIMITS,
-  REGISTER_TOAST_MESSAGES,
-  RegisterForm,
-} from '@/entity/register'
+import { FILE_UPLOAD_LIMITS, RegisterForm } from '@/entity/register'
 import { formatDaumAddress } from '@/shared/utils/format-daum-address'
 import { InputField } from '@/widgets/input-field'
 
+import { REGISTER_TOAST_MESSAGES } from '../register.constant'
 import { useCheckStoreNameMutation } from './check-store-name.mutation'
 import { ConfirmStoreAlert } from './confirm-store-alert.ui'
 import { DuplicateStoreAlert } from './duplicate-store-alert.ui'
@@ -162,6 +159,7 @@ export function StoreInfo() {
           placeholder="스토어를 검색해주세요"
           buttonText="검색"
           readOnly
+          allowEmptyButtonClick
           value={storeName}
           onButtonClick={() => setSearchOpen(true)}
           error={!!errors.storeName}
