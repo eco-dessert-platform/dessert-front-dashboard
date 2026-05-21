3e9caab95fbbd6f036ca52913340719e86604b7b
jieunsse <jieunsse@gmail.com>
2026-05-22 01:16:40 +0900

    [seller] fix(171): emailDomainSelection Resolver 타입 불일치 수정

--- apps/seller/src/features/seller-info/store-info-form/store-info-form.ui.tsx
+++ apps/seller/src/features/seller-info/store-info-form/store-info-form.ui.tsx
@@ -24,6 +24,7 @@ export function StoreInfoForm() {
       subPhoneNumber: '',
       emailLocal: '',
       emailDomain: '',
+      emailDomainSelection: '',
       originAddress: '',
       originAddressDetail: '',
     },
