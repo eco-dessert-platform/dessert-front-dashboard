a4fa8c2b4f1f24268d5146490955f937da7ed580
yrkim <dudfhr132@naver.com>
2026-06-08 23:09:41 +0900

    fix(layout): set min width to layout property

--- apps/seller/src/pages/settlements/layout.tsx
+++ apps/seller/src/pages/settlements/layout.tsx
@@ -1,6 +1,6 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="-mx-[90px] -my-40 flex min-h-[calc(100vh-80px)] flex-col gap-10 p-40">
+    <div className="-mx-[90px] -my-40 flex min-h-[calc(100vh-80px)] min-w-[700px] flex-col gap-10 p-40">
       {children}
     </div>
   )
